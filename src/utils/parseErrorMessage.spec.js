8573b03bae9e1ac283629d108e5c2ad5ece3a7b2
Jan Bevers <jan.bevers@kunstmaan.be>
2018-04-19 15:47:39 +0200

    Use more generic test data

--- src/utils/parseErrorMessage.spec.js
+++ src/utils/parseErrorMessage.spec.js
@@ -4,38 +4,38 @@ test('Parses an error string with a custom error object correctly', () => {
     const message =
         'error executing chaincode: transaction returned with failure: ' +
         '[clientchannel-797e3444]Calling chaincode Invoke() returned error response ' +
-        '[{"message":"Invalid data!","key":"invalid_data","data":{"property":"bondData"}}]. ' +
+        '[{"message":"Invalid data!","key":"invalid_data","data":{"property":"objectData"}}]. ' +
         'Sending ERROR message back to peer';
     const error = parseErrorMessage(message);
     expect(error).toBeInstanceOf(Error);
     expect(error.message).toBe('Invalid data!');
     expect(error.key).toBe('invalid_data');
-    expect(error.data).toEqual({property: 'bondData'});
+    expect(error.data).toEqual({property: 'objectData'});
 });
 
 test('Parses an error string with a custom error object and no message correctly (invoke)', () => {
     const message =
         'error executing chaincode: transaction returned with failure: ' +
         '[clientchannel-797e3444]Calling chaincode Invoke() returned error response ' +
-        '[{"key":"invalid_data","data":{"property":"bondData"}}]. Sending ERROR message back to peer';
+        '[{"key":"invalid_data","data":{"property":"objectData"}}]. Sending ERROR message back to peer';
     const error = parseErrorMessage(message);
     expect(error).toBeInstanceOf(Error);
     expect(error.message).toBe('Unknown error');
     expect(error.key).toBe('invalid_data');
-    expect(error.data).toEqual({property: 'bondData'});
+    expect(error.data).toEqual({property: 'objectData'});
 });
 
 test('Parses an error string with a custom error object and no message correctly (query)', () => {
     const message =
         '2 UNKNOWN: error executing chaincode: transaction returned with failure: ' +
-        '{"key":"unknown_entity","data":{"property":"bondId","value":"0068E00000CqlVHQAZ"},' +
-        '"stack":"Error: unknown_entity\n    at getBondHelper (/opt/gopath/src/chaincode/bond/chaincode.js:471:15)\n' +
+        '{"key":"unknown_entity","data":{"property":"id","value":"my-id"},' +
+        '"stack":"Error: unknown_entity\n    at getId (/opt/gopath/src/chaincode/fabcar/chaincode.js:32:85)\n' +
         '    at <anonymous>\n    at process._tickCallback (internal/process/next_tick.js:188:7)"}';
     const error = parseErrorMessage(message);
     expect(error).toBeInstanceOf(Error);
     expect(error.message).toBe('Unknown error');
     expect(error.key).toBe('unknown_entity');
-    expect(error.data).toEqual({property: 'bondId', value: '0068E00000CqlVHQAZ'});
+    expect(error.data).toEqual({property: 'id', value: 'my-id'});
 });
 
 test('Parses an error string with a default error object correctly', () => {
